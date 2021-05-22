2549d61eb4113885bda0b51e327e99a1d8f2e722
Nicolas Alphonso <nicolas.alphonso@gmail.com>
2021-05-22 19:54:17 +0200

    Déplacement du joueur terminé

--- main.py
+++ main.py
@@ -27,10 +27,12 @@ while loop:  # Boucle d'événements
 	ecran.blit(game.player.image, game.player.rect)
 
 	# vérifier si joueur souhaite aller à gaucher ou à droite
-	if game.pressed.get(pygame.K_RIGHT):
+	if game.pressed.get(pygame.K_RIGHT) and game.player.rect.x + game.player.rect.width < ecran.get_width():
 		game.player.move_right()
-	elif game.pressed.get(pygame.K_LEFT):
+	elif game.pressed.get(pygame.K_LEFT) and game.player.rect.x > 0:
 		game.player.move_left()
+
+	print(game.player.rect.x)
 
 	pygame.display.flip()  # L'affichage devient effectif - l'ecran est mis à jour.
 
